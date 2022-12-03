27718de717cf1d7b0cafdaedf77a13e07e1044c3
osifunny <kasugashikuro@gmail.com>
2022-12-03 14:20:00 +0700

    Enemy Pattern fly

---
+++ Assets/Scripts/Enemy/EnemyPatternFly.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyPatternFly : MonoBehaviour
+{
+    public AIPath aiPath;
+
+    void Update(){
+        if(aiPath.desiredVelocity.x >= 0.01f) transform.localScale = new Vector3(-1f,1f,1f);
+        else if(aiPath.desiredVelocity.x <= 0.01f) transform.localScale = new Vector3(1f,1f,1f);
+    }
+}
